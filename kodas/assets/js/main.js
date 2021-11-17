634f756b134850525a464f1187c116f8bdc54d0c
Arvydas <arvydasbliujus@gmail.com>
2021-11-17 16:50:58 +0200

    added classwork content & text

--- kodas/assets/js/main.js
+++ kodas/assets/js/main.js
@@ -4,8 +4,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector(".header").style.fontSize = "30px";
+    document.querySelector(".header").style.fontSize = "0.8rem";
   } else {
-    document.querySelector(".header").style.fontSize = "50px";
+    document.querySelector(".header").style.fontSize = "1.8rem";
   }
 }
